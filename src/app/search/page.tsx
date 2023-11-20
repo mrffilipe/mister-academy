b887e343604c94f40d0f6107c72ab474f2b3b3de
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-11-20 17:08:27 -0300

    Adjust Search component

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,12 +3,44 @@ import styles from './styles.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import MockImg from '@/assets/img/img_mock.jpg'
+
+const Item = () => {
+    return (
+        <li className={styles.item}>
+            <Link href=''>
+                <div className={styles.banner}>
+                    <Image src={MockImg} alt='' />
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.item_header}>
+                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce libero ex, vehicula quis varius sit amet, ullamcorper non metus. Nam eget tortor eget ex efficitur viverra.</p>
+                    </div>
+                    <div className={styles.item_content}>
+                        <ul className={styles.item_tags}>
+                            <li>C#</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                        </ul>
+                        <div className={styles.item_stats}>
+                            <span className={styles.author}>Filipe M. Ferracioli</span>
+                            <span className={styles.views}>1.4m</span>
+                            <span className={styles.date}>18/11/2023 às 21:08</span>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </li>
+    )
+}
+
 const Search = () => {
     return (
         <section className={styles.search}>
-            <h2>Resultados da busca</h2>
+            <h3>Resultados da busca</h3>
             <div className={styles.search_header}>
-                <span>500 resultados</span>
+                <span>8 resultados</span>
                 <div className={styles.order_by}>
                     <select>
                         <option value="">Data</option>
@@ -19,30 +51,30 @@ const Search = () => {
             </div>
             <div className={styles.results}>
                 <ul>
-                    <li className={styles.item}>
-                        <Link href=''>
-                            <Image src='' alt='' />
-                            <div className={styles.info}>
-                                <div className={styles.item_header}>
-                                    <h4>Título</h4>
-                                    <p>Resumo</p>
-                                </div>
-                                <div className={styles.item_content}>
-                                    <ul className={styles.item_tags}>
-                                        <li>Tag</li>
-                                        <li>Tag</li>
-                                        <li>Tag</li>
-                                    </ul>
-                                    <div className={styles.item_stats}>
-                                        <span>Autor</span>
-                                        <span>Vizualizações</span>
-                                        <span>Data</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </ul>
+            </div>
+            <div className={styles.navigation}>
+                <Link href=''>Anterior</Link>
+                <ul>
+                    <li>
+                        <Link href=''>1</Link>
+                    </li>
+                    <li>
+                        <Link href=''>2</Link>
+                    </li>
+                    <li>
+                        <Link href=''>3</Link>
                     </li>
                 </ul>
+                <Link href=''>Próximo</Link>
             </div>
         </section>
     )
